c96997f7e948084c9ef7bed5c078f6162d42d667
owen-me <owenkim.dev@gmail.com>
2022-06-16 20:19:19 +0900

    add mock test

---
+++ src/mock/product/test/product_service_no_di.test.js
@@ -0,0 +1,28 @@
+const ProductService = require('../product_service_no_di.js');
+const ProductClient = require('../product_client.js');
+
+jest.mock('../product_client');
+
+describe('ProductService', () => {
+  const fetchItems = jest.fn(async () => [
+    { item: 'Milk', available: true },
+    { item: 'Banana', available: false },
+  ]);
+
+  ProductClient.mockImplementation(() => {
+    fetchItems;
+  });
+
+  let productService;
+
+  beforeEach(() => {
+    productService = new ProductService();
+  });
+
+  it('should filter out only available items', async () => {
+    const items = await productService.fetchAvailableItems();
+
+    expect(items.length).toBe(1);
+    expect(items).toEqual([{ item: 'Milk', available: true }]);
+  });
+});
